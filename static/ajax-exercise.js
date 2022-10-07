9476b4e0115fc482b5ea7c9152092763542913e0
Carina Ogbat <carinawrightogbat@gmail.com>
2022-10-06 21:05:33 -0700

    almost done with order melons

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -36,6 +36,35 @@ document.querySelector('#weather-form').addEventListener('submit', showWeather);
 function orderMelons(evt) {
   evt.preventDefault();
 
+  const formInputs = {
+    melon_type: document.getElementById('melon-type-field').value,
+    qty: document.getElementById('qty-field').value,
+  };
+
+  fetch('/order-melons.json', {
+    method: 'POST',
+    body: JSON.stringify(formInputs),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+
+  })
+    .then((response) => response.json())
+    .then((responseJson) => {     
+      
+      // put const and if else for error message
+
+      document.querySelector('#order-status').innerHTML = responseJson['msg'];
+      // document.querySelector('#order-status').insertadjacentHTML('afterbegin', responseJson['code'])
+
+      document.querySelector('#order-status')
+      .classList.add('.order-error');
+    });
+    
+
+
+    
+ 
   // TODO: show the result message after your form
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
